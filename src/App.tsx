91fbe8414291ef6379096b2f6ebf597597ebbe3d
Coukrafter <40741268+Coukrafter@users.noreply.github.com>
2023-01-27 21:55:17 +0100

    added router and created page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { DashboardPage, FaqPage, ListPage, NotFoundPage } from "src/pages";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/faq" element={<FaqPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as DashboardPage } from "./DashboardPage/DashboardPage";
+export { default as FaqPage } from "./FaqPage/FaqPage";
+export { default as ListPage } from "./ListPage/ListPage";
+export { default as NotFoundPage } from "./NotFoundPage/NotFoundPage";
