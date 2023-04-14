ea9ee068830bac7d1d4c2e79adc57f26083ea263
Jakub Kniebügl <jakubkniebugl@Jakubs-MBP.localdomain>
2023-04-14 10:14:42 +0200

    replaced predefined colors with tailwind colors and updated buttons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,8 @@
-const colors = require("tailwindcss/colors");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          main: "#9999ff",
-          alt: "#6666dd",
-          text: "#303030",
-        },
-        secondary: {
-          main: "#555555",
-          alt: "#2b2d42",
-          text: "#e1e1e1",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
