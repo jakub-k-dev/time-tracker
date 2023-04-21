61189f763090362a679bca98417684af82a3e106
Jakub Kniebügl <jakubkniebugl@Jakubs-MBP.localdomain>
2023-04-21 13:56:52 +0200

    implemented breadcrumbs #8

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,65 @@
+import { HomeIcon } from "@heroicons/react/20/solid";
+import { ReactNode } from "react";
+
+const breadcrumbNameMap: Record<string, ReactNode> = {
+  "/": <HomeIcon className="h-5 w-5 flex-shrink-0" />,
+  "/list": "List",
+  "/faq": "Faq",
+};
+
+type Props = {
+  paths: string[];
+};
+
+export default function Breadcrumbs({ paths }: Props) {
+  return (
+    <nav
+      className="flex border-b border-gray-200 bg-white"
+      aria-label="Breadcrumb"
+    >
+      <p className="mx-auto flex w-full max-w-screen-xl space-x-4 px-4 sm:px-6 lg:px-8 h-11">
+        {paths.map((path, index) => {
+          const isLast = index === paths.length - 1;
+          if (index === 0) {
+            return (
+              <div
+                key="/"
+                className="flex items-center text-gray-400 hover:text-gray-500 last:text-gray-700 last:hover:text-gray-900"
+              >
+                <a href="/">
+                  {breadcrumbNameMap["/"]}
+                  <span className="sr-only">Home</span>
+                </a>
+              </div>
+            );
+          }
+          const href = paths.slice(1, index + 1).join("");
+
+          return (
+            <div
+              key={href}
+              className="flex items-center font-medium last:font-bold"
+            >
+              <svg
+                className="h-full w-6 flex-shrink-0 text-gray-200"
+                viewBox="0 0 24 44"
+                preserveAspectRatio="none"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+              </svg>
+              <a
+                href={href}
+                className="ml-4 text-sm"
+                aria-current={isLast ? "page" : undefined}
+              >
+                {breadcrumbNameMap[path] || path.substring(1)}
+              </a>
+            </div>
+          );
+        })}
+      </p>
+    </nav>
+  );
+}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,7 @@
 import { ReactNode } from "react";
+import { useLocation } from "react-router-dom";
+
+import Breadcrumbs from "../Breadcrumbs";
 
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer";
@@ -8,9 +11,12 @@ type Props = {
 };
 
 export default function Layout({ children }: Props) {
+  const { pathname } = useLocation();
+  const paths = ["/", ...(pathname.match(/(\/[^/]+)/g) || [])];
   return (
     <>
       <Navbar />
+      <Breadcrumbs paths={paths} />
       <div className="py-10 flex flex-col gap-8 flex-grow">{children}</div>
       <Footer />
     </>
