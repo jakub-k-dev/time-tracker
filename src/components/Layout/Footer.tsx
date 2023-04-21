c8082511e949b0e3791ef0e9fe3ce256ed44c3e0
Jakub Kniebügl <jakubkniebugl@Jakubs-MBP.localdomain>
2023-04-21 14:25:00 +0200

    social links now open in new tab #18

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -80,6 +80,7 @@ export default function Footer() {
               key={item.name}
               href={item.href}
               className="text-gray-400 hover:text-gray-500"
+              target="_blank"
             >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
