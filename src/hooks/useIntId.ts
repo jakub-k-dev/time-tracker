ff1a588697b6ae2fbabef95acfee661107ab9360
Jakub Kniebügl <jakubkniebugl@Jakubs-MacBook-Pro.local>
2023-05-02 22:02:33 +0200

    simplified useIntId hook

--- src/hooks/useIntId.ts
+++ src/hooks/useIntId.ts
@@ -2,10 +2,6 @@ import { useRef } from "react";
 
 export const useIntId = () => {
   const id = useRef(0);
-  const getNewId = () => {
-    id.current += 1;
-    return id.current;
-  };
 
-  return getNewId;
+  return () => ++id.current;
 };
